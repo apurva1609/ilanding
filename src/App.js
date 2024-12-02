172ccbba208c4291334cd12e5e0301bfa614f95f
apurva1609 <apurvapatil268@gmail.com>
2024-12-02 11:32:24 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,20 +7,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Contact from './Components/Contact';
 import Pricing from './Components/Pricing';
 import Footer from './Components/Footer';
+import Hero from './Components/Hero';
 // import ILanding from "./Assets/Css/ILanding.css";
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
+// import { Navbar } from 'react-bootstrap';
+import MyNavbar from './Components/MyNavbar';
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar/>
+        {/* <Navbar/> */}
+        <MyNavbar/>
+        <Hero/>
           <Routes>
+            {/* <Route path='/Home' element={<Hero/>}/> */}
             <Route path='/Pricing' element={<Pricing/>}/>
             <Route path='/Contact' element={<Contact/>}/>
           </Routes>
         <Footer/>
      </BrowserRouter>
+     {/* <Footer/> */}
      
     </>
    
